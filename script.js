f1443bf0fdd0e43a40ece107608ef271e68173cd
Tofig Amraslanov <tofiqamraslanov@gmail.com>
2020-12-21 03:36:09 +0400

    implementing reset button

--- script.js
+++ script.js
@@ -3,8 +3,8 @@
 // Selecting Elements
 const player0El = document.querySelector('.player--0 ');
 const player1El = document.querySelector('.player--1 ');
-const score0EL = document.querySelector('#score--0');
-const score1EL = document.getElementById('score--1');
+const score0El = document.querySelector('#score--0');
+const score1El = document.getElementById('score--1');
 const current0El = document.getElementById('current--0');
 const current1El = document.getElementById('current--1');
 const diceEl = document.querySelector('.dice');
@@ -12,15 +12,28 @@ const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
-// Starting conditions
-score0EL.textContent = 0;
-score1EL.textContent = 0;
-diceEl.classList.add('hidden');
+let scores, currentScore, activePLayer, playing;
 
-const scores = [0, 0];
-let currentScore = 0;
-let activePLayer = 0;
-let playing = true;
+// Starting conditions
+const init = function () {
+  scores = [0, 0];
+  currentScore = 0;
+  activePLayer = 0;
+  playing = true;
+
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  current0El.textContent = 0;
+  current0El.textContent = 0;
+
+  diceEl.classList.add('hidden');
+  player0El.classList.remove('player--winner');
+  player1El.classList.remove('player--winner');
+
+  player0El.classList.add('player--active');
+  player1El.classList.remove('player--active');
+};
+init();
 
 const switchPlayer = function () {
   document.getElementById(`current--${activePLayer}`).textContent = 0;
@@ -30,6 +43,7 @@ const switchPlayer = function () {
   player0El.classList.toggle('player--active');
   player1El.classList.toggle('player--active');
 };
+
 // Rolling dice functionality
 btnRoll.addEventListener('click', function () {
   if (playing) {
@@ -75,3 +89,6 @@ btnHold.addEventListener('click', function () {
     }
   }
 });
+
+// Reset game
+btnNew.addEventListener('click', init);
