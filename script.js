4d5f8f8c706a51b45fe3c2da575bfd12d9d4b221
Tofig Amraslanov <tofiqamraslanov@gmail.com>
2020-12-21 01:09:45 +0400

    Changed

--- script.js
+++ script.js
@@ -3,3 +3,6 @@
 // Selecting Elements
 const score0EL = document.querySelector('#score--0');
 const score1EL = document.getElementById('score--1');
+
+score0EL.textContent = 0;
+score1EL.textContent = 0;
